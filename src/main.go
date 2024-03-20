fe0a172241e8f6c68d098fa242b0e1071ec5c9ce
hungdoo <nhuthung.dong@gmail.com>
2024-03-20 15:00:42 +0700

    up listener

--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/hungdoo/bot/src/packages/db"
 	"github.com/hungdoo/bot/src/packages/dotenv"
+	"github.com/hungdoo/bot/src/packages/log"
 	"github.com/hungdoo/bot/src/services/telecommands"
 )
 
@@ -17,6 +20,15 @@ func main() {
 
 	if dotenv.GetEnv("SERVICE_MODE") == "listener" {
 		// go commandService.Work()
+		go func() { // listener only refreshes jobs
+			for {
+				_, err := commandService.Factory.GetJobs()
+				if err != nil {
+					log.GeneralLogger.Printf("GetJobs err: [%s]", err)
+				}
+				time.Sleep(time.Second * 30)
+			}
+		}()
 		commandService.ListenToCommand()
 	} else {
 		commandService.Work()
